ed6340ad13b355ac7e8ceddeef66667f6c16e59f
Matt Gacy <matt@mgacy.com>
2022-09-14 07:46:52 -0700

    Configuration

--- Sources/Lytics/Lytics.swift
+++ Sources/Lytics/Lytics.swift
@@ -7,5 +7,17 @@
 import Foundation
 
 public final class Lytics {
+
+    public static let shared: Lytics = {
+        let instance = Lytics()
+        // setup code
+        return instance
+    }()
+
+    public private(set) var hasStarted: Bool = false
+
+    public func start(_ configuration: (LyticsConfiguration) -> Void) {
+        // ...
+    }
 }
 
---
+++ Sources/Lytics/LyticsConfiguration.swift
@@ -0,0 +1,10 @@
+//
+//  LyticsConfiguration.swift
+//
+//  Created by Mathew Gacy on 9/12/22.
+//
+
+import Foundation
+
+public struct LyticsConfiguration: Equatable {
+}
