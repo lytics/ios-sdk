1b37d267f93041d6e0e9ae34519ca0e669618ba3
Matt Gacy <matt@mgacy.com>
2022-09-18 20:47:03 -0700

    User

---
+++ Sources/Lytics/LyticsUser.swift
@@ -0,0 +1,34 @@
+//
+//  LyticsUser.swift
+//
+//  Created by Mathew Gacy on 9/12/22.
+//
+
+import Foundation
+
+/// A representation of the user.
+public struct LyticsUser: Codable, Equatable {
+
+    public enum UserType: Codable, Equatable {
+        case anonymous
+        case identified
+    }
+
+    /// Valuable identification fields of an individual.
+    var identifiers: [String: String]
+
+    /// Additional information about a user.
+    var traits: [String: String]
+
+    /// Initializes a user.
+    /// - Parameters:
+    ///   - identifiers: Valuable identification fields of an individual.
+    ///   - traits: Additional information about a user.
+    public init(
+        identifiers: [String : String] = [:],
+        traits: [String : String] = [:]
+    ) {
+        self.identifiers = identifiers
+        self.traits = traits
+    }
+}
