33963aaeba93f406099c78a912be2cb98ee572f6
Matt Gacy <matt@mgacy.com>
2022-09-30 13:05:02 -0700

    Remove Decodable conformance, rename

--- Sources/Lytics/Extensions/Never+Encodable.swift
+++ Sources/Lytics/Extensions/Never+Encodable.swift
@@ -1,16 +1,12 @@
 //
-//  Never+Codable.swift
+//  Never+Encodable.swift
 //
 //  Created by Mathew Gacy on 9/19/22.
 //
 
 import Foundation
 
-extension Never: Codable {
-    public init(from decoder: Decoder) throws {
-        fatalError()
-    }
-
+extension Never: Encodable {
     public func encode(to encoder: Encoder) throws {
         // no-op
     }
