edfd4e2fe94a1a13005afa2d5e0b7febb8f70153
Matt Gacy <matt@mgacy.com>
2022-10-03 18:47:17 -0700

    Specify types, add default values, logLevel

--- Sources/Lytics/LyticsConfiguration.swift
+++ Sources/Lytics/LyticsConfiguration.swift
@@ -10,16 +10,16 @@ import Foundation
 public struct LyticsConfiguration: Equatable {
 
     /// Lytics account API token.
-    public var apiKey: String
+    public var apiKey: String = ""
 
     /// Default stream name to which events will be sent if not explicitly set for an event.
-    public var defaultStream: String
+    public var defaultStream: String = ""
 
     /// The key that represents the core identifier to be used in api calls.
-    public var primaryIdentityKey = "_uid"
+    public var primaryIdentityKey: String = "_uid"
 
     /// The key which we use to store the anonymous identifier.
-    public var anonymousIdentityKey = "_uid"
+    public var anonymousIdentityKey: String = "_uid"
 
     /// A Boolean value indicating whether application lifecycle events should be tracked automatically.
     public var trackApplicationLifecycleEvents: Bool = false
@@ -34,19 +34,22 @@ public struct LyticsConfiguration: Equatable {
     public var trackDeepLinks: Bool = false
 
     /// The interval in seconds at which the event queue is uploaded to the Lytics API.
-    public var uploadInterval = 10
+    public var uploadInterval: Double = 10
 
     /// The max size of the event queue before forcing an upload of the event queue to the Lytics API.
     ///
     /// Set to `0` to disable.
-    public var maxQueueSize = 10
+    public var maxQueueSize: Int = 10
 
     /// Session timeout in seconds.
     ///
     /// This is the period from when the app enters the background and the session expires, starting a new session.
-    public var sessionDuration = 1200
+    public var sessionDuration: Double = 1200
 
     /// Enable sandbox mode which adds a "sandbox" flag to all outbound events. This flag then enables those events to be
     /// processed in an alternative way or skipped entirely upon delivery to the Lytics collection APIs.
     public var enableSandbox: Bool = false
+
+    /// Set the logging level of the SDK.
+    public var logLevel: LogLevel = .error
 }
