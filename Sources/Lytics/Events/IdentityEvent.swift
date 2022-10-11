4b406b2e29d01da30499a5319e109290b266ca37
Matt Gacy <matt@mgacy.com>
2022-10-11 08:16:22 -0700

    Add AnyCodable

--- Sources/Lytics/Events/IdentityEvent.swift
+++ Sources/Lytics/Events/IdentityEvent.swift
@@ -11,4 +11,17 @@ struct IdentityEvent<I: Encodable, A: Encodable>: Encodable {
     var name: String?
     var identifiers: I?
     var attributes: A?
+
+    init(stream: String, name: String? = nil, identifiers: I? = nil, attributes: A? = nil) {
+        self.stream = stream
+        self.name = name
+        self.identifiers = identifiers
+        self.attributes = attributes
+    }
+
+    private enum CodingKeys: CodingKey {
+        case name
+        case identifiers
+        case attributes
+    }
 }
--- Sources/Lytics/LyticsUser.swift
+++ Sources/Lytics/LyticsUser.swift
@@ -4,6 +4,7 @@
 //  Created by Mathew Gacy on 9/12/22.
 //
 
+import AnyCodable
 import Foundation
 
 /// A representation of the user.
@@ -18,10 +19,10 @@ public struct LyticsUser: Codable, Equatable {
     public var userType: UserType
 
     /// Valuable identification fields of an individual.
-    public var identifiers: [String: String]
+    public var identifiers: [String: AnyCodable]
 
     /// Additional information about a user.
-    public var attributes: [String: String]
+    public var attributes: [String: AnyCodable]
 
     /// Initializes a user.
     /// - Parameters:
@@ -30,11 +31,28 @@ public struct LyticsUser: Codable, Equatable {
     ///   - attributes: Additional information about a user.
     public init(
         userType: UserType = .anonymous,
-        identifiers: [String : String] = [:],
-        attributes: [String : String] = [:]
+        identifiers: [String : AnyCodable] = [:],
+        attributes: [String : AnyCodable] = [:]
     ) {
         self.userType = userType
         self.identifiers = identifiers
         self.attributes = attributes
+    }
+}
+
+public extension LyticsUser {
+    /// Initializes a user.
+    /// - Parameters:
+    ///   - userType: The type of user.
+    ///   - identifiers: Valuable identification fields of an individual.
+    ///   - attributes: Additional information about a user.
+    init(
+        userType: UserType = .anonymous,
+        identifiers: [String : Any] = [:],
+        attributes: [String : Any] = [:]
+    ) {
+        self.userType = userType
+        self.identifiers = identifiers.mapValues(AnyCodable.init(_:))
+        self.attributes = attributes.mapValues(AnyCodable.init(_:))
     }
 }
