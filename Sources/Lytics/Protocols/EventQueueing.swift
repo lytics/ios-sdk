8742d81f5da0bb1a685cbe74c6cf487c0eab78e2
Matt Gacy <matt@mgacy.com>
2022-10-18 20:00:17 -0700

    Documentation

--- Sources/Lytics/Protocols/EventQueueing.swift
+++ Sources/Lytics/Protocols/EventQueueing.swift
@@ -6,8 +6,13 @@
 
 import Foundation
 
+/// A type that holds a queue of events.
 protocol EventQueueing: Actor {
+
+    /// Adds an event to the queue.
+    /// - Parameter event: the event to add.
     func enqueue<E: StreamEvent>(_ event: E)
 
+    /// Sends all queued events.
     func flush()
 }
