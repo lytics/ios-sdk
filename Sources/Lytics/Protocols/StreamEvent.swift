af147c56a66e34eb88953637c001d5a0fb1c66dc
Matt Gacy <matt@mgacy.com>
2022-10-19 07:45:39 -0700

    Add name, documentation

--- Sources/Lytics/Protocols/StreamEvent.swift
+++ Sources/Lytics/Protocols/StreamEvent.swift
@@ -6,6 +6,12 @@
 
 import Foundation
 
+/// A class of types to be added to an event stream.
 protocol StreamEvent: Encodable {
+
+    /// The stream to which the event is destined.
     var stream: String { get }
+
+    /// The event name.
+    var name: String? { get }
 }
