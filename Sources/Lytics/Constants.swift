ad0b02999673226e1dacb66c22b6601992d5dd27
Matt Gacy <matt@mgacy.com>
2022-10-23 16:13:35 -0700

    Add missing Constants

---
+++ Sources/Lytics/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//
+//  Created by Mathew Gacy on 10/20/22.
+//
+
+import Foundation
+
+enum Constants {
+    static let baseDirectory = "com.lytics.ios-sdk"
+    static let requestStorageDirectory = "requests"
+    static let requestStorageFilename = "requests"
+}
