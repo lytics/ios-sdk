87a81d385c2fa32ee66a0ad4052c0320d1034585
Matt Gacy <matt@mgacy.com>
2022-10-23 17:09:43 -0700

    Additional test helpers

--- Tests/LyticsTests/Mocks/Mock.swift
+++ Tests/LyticsTests/Mocks/Mock.swift
@@ -9,6 +9,8 @@ import AnyCodable
 import Foundation
 
 enum Mock {
+    static let apiKey = "at.xxxx"
+
     static let consentEvent = ConsentEvent(
         identifiers: User1.identifiers,
         attributes: User1.attributes,
@@ -29,6 +31,38 @@ enum Mock {
     static let url = URL(string: "https://api.lytics.io/collect/json/stream")!
 
     static let timestamp: Millisecond = 1666000000000
+}
+
+extension Mock {
+    enum Name: String {
+        case one = "name_1"
+        case two = "name_2"
+        case three = "name_3"
+    }
+
+    enum Stream: String {
+        case one = "stream_1"
+        case two = "stream_2"
+        case three = "stream_3"
+    }
+
+    enum Timestamp: Millisecond {
+        case one = 1_666_000_000_000
+        case two = 1_666_000_001_000
+        case three = 1_666_000_002_500
+    }
+
+    func name(_ value: Name) -> String {
+        value.rawValue
+    }
+
+    func stream(_ value: Stream) -> String {
+        value.rawValue
+    }
+
+    func timestamp(_ value: Timestamp) -> Millisecond {
+        value.rawValue
+    }
 }
 
 extension Mock {
