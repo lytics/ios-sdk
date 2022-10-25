d6901678023061aae10a66fc7f954bbd4dc5d403
Matt Gacy <matt@mgacy.com>
2022-10-25 12:46:10 -0700

    Assets

---
+++ Examples/SwiftUIDemo/SwiftUIDemo/Extensions/Image+Assets.swift
@@ -0,0 +1,16 @@
+//
+//  Image+Assets.swift
+//  SwiftUIDemo
+//
+//  Created by Mathew Gacy on 10/24/22.
+//  Copyright © 2022 Lytics. All rights reserved.
+//
+
+import SwiftUI
+
+public extension Image {
+    static let image2 = Image("image2", bundle: .main)
+    static let image3 = Image("image3", bundle: .main)
+    static let image4 = Image("image4", bundle: .main)
+    static let image5 = Image("image5", bundle: .main)
+}
