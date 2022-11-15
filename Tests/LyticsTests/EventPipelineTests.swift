8af2833d17c8c32ca59871a7a3a5f816e4d4c608
Matt Gacy <matt@mgacy.com>
2022-11-15 13:47:04 -0700

    Additional tests

--- Tests/LyticsTests/EventPipelineTests.swift
+++ Tests/LyticsTests/EventPipelineTests.swift
@@ -35,4 +35,64 @@ final class EventPipelineTests: XCTestCase {
         await sut.event(stream: expectedName, timestamp: 0, name: nil, event: Mock.event)
         XCTAssertEqual(enqueuedEvent.stream, expectedName)
     }
+
+    func testWillSendIfNotRequireConsent() async throws {
+        var enqueuedEvent: StreamEvent!
+        let eventQueue = EventQueueMock(onEnqueue: { event in
+            enqueuedEvent = event
+        })
+
+        let sut = EventPipeline(
+            configuration: .init(
+                defaultStream: "",
+                requireConsent: false),
+            logger: .mock,
+            sessionDidStart: { _ in false },
+            eventQueue: eventQueue,
+            uploader: UploaderMock<DataUploadResponse>(),
+            userSettings: .optedOutMock)
+
+        await sut.event(stream: nil, timestamp: 0, name: nil, event: Mock.event)
+        XCTAssertEqual(enqueuedEvent.stream, Constants.defaultStream)
+    }
+
+    func testWillSendIfRequireConsentAndOptedIn() async throws {
+        var enqueuedEvent: StreamEvent!
+        let eventQueue = EventQueueMock(onEnqueue: { event in
+            enqueuedEvent = event
+        })
+
+        let sut = EventPipeline(
+            configuration: .init(
+                defaultStream: "",
+                requireConsent: true),
+            logger: .mock,
+            sessionDidStart: { _ in false },
+            eventQueue: eventQueue,
+            uploader: UploaderMock<DataUploadResponse>(),
+            userSettings: .optedInMock)
+
+        await sut.event(stream: nil, timestamp: 0, name: nil, event: Mock.event)
+        XCTAssertEqual(enqueuedEvent.stream, Constants.defaultStream)
+    }
+
+    func testWillNotSendIfRequireConsentAndOptedOut() async throws {
+        var enqueuedEvent: StreamEvent?
+        let eventQueue = EventQueueMock(onEnqueue: { event in
+            enqueuedEvent = event
+        })
+
+        let sut = EventPipeline(
+            configuration: .init(
+                defaultStream: "",
+                requireConsent: true),
+            logger: .mock,
+            sessionDidStart: { _ in false },
+            eventQueue: eventQueue,
+            uploader: UploaderMock<DataUploadResponse>(),
+            userSettings: .optedOutMock)
+
+        await sut.event(stream: nil, timestamp: 0, name: nil, event: Mock.event)
+        XCTAssertNil(enqueuedEvent)
+    }
 }
