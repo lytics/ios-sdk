eb7504af198f6cca35aaf4de10a729045144c521
Matt Gacy <matt@mgacy.com>
2023-01-09 15:08:11 -0700

    Define Entity model

---
+++ Sources/Lytics/Networking/Entity.swift
@@ -0,0 +1,50 @@
+//
+//  Entity.swift
+//
+//  Created by Mathew Gacy on 1/4/23.
+//
+
+import AnyCodable
+import Foundation
+
+struct Entity: Codable, Equatable {
+
+    struct Meta: Codable, Equatable {
+        var byFields: [String]
+        var format: String
+        var name: String
+
+        init(
+            byFields: [String],
+            format: String,
+            name: String
+        ) {
+            self.byFields = byFields
+            self.format = format
+            self.name = name
+        }
+
+        private enum CodingKeys: String, CodingKey {
+            case byFields = "by_fields"
+            case format
+            case name
+        }
+    }
+
+    var data: AnyCodable
+    var message: String
+    var meta: Meta
+    var status: Int
+
+    init(
+        data: AnyCodable,
+        message: String,
+        meta: Meta,
+        status: Int
+    ) {
+        self.data = data
+        self.message = message
+        self.meta = meta
+        self.status = status
+    }
+}
