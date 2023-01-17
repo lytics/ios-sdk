3d0ae6f7f24532cd8e45c7c6f060dbcc26f33bad
Matt Gacy <matt@mgacy.com>
2023-01-17 10:00:24 -0700

    Remove dead code

--- Tests/LyticsTests/UserSettingsTests.swift
+++ Tests/LyticsTests/UserSettingsTests.swift
@@ -32,29 +32,4 @@ final class UserSettingsTests: XCTestCase {
         let actual = defaults.bool(for: .didOptIn)
         XCTAssertEqual(actual, expected)
     }
-
-
-//    func readOptIn() {
-//        let expected = false
-//
-//        let defaults = UserDefaults()
-//
-//        let sut = UserSettings.live(userDefaults: defaults)
-//        sut.setOptIn(expected)
-//
-//        let actual = defaults.bool(for: .didOptIn)
-//        XCTAssertEqual(actual, expected)
-//    }
-//
-//    func testWriteOptIn() {
-//        let expected = true
-//
-//        let defaults = UserDefaults()
-//        defaults.set(expected, for: .didOptIn)
-//
-//        let sut = UserSettings.live(userDefaults: defaults)
-//
-//        let actual = sut.getOptIn()
-//        XCTAssertEqual(actual, expected)
-//    }
 }
