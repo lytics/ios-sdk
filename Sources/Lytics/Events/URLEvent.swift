f13b991cb5cd1c8071ad4dc2bc27b4ca4f1916fe
Matt Gacy <matt@mgacy.com>
2023-01-31 09:03:10 -0700

    Use String for options key

--- Sources/Lytics/Events/URLEvent.swift
+++ Sources/Lytics/Events/URLEvent.swift
@@ -4,11 +4,24 @@
 //  Created by Mathew Gacy on 12/4/22.
 //
 
+import class UIKit.UIApplication
 import AnyCodable
 import Foundation
 
 struct URLEvent: Codable, Equatable {
     var url: URL
-    var options: [AnyCodable: AnyCodable]?
+    var options: [String: AnyCodable]?
     var identifiers: [String: AnyCodable]?
+}
+
+extension URLEvent {
+    internal init(
+        url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any]? = nil,
+        identifiers: [String : AnyCodable]? = nil
+    ) {
+        self.url = url
+        self.options = options != nil ? Dictionary(uniqueKeysWithValues: options!.map { ($0.rawValue, AnyCodable($1)) }) : nil
+        self.identifiers = identifiers
+    }
 }
