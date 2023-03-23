95c8632965c7ae873611e1bd11713f16da37bd65
Matt Gacy <matt@mgacy.com>
2023-03-23 15:49:28 -0700

    Make plugin dependency conditional

--- Package.swift
+++ Package.swift
@@ -21,9 +21,6 @@ let package = Package(
             url: "https://github.com/Flight-School/AnyCodable",
             from: "0.6.7"),
         .package(
-            url: "https://github.com/mobelux/swift-version-file-plugin",
-            from: "0.1.0"),
-        .package(
             url: "https://github.com/nicklockwood/SwiftFormat",
             from: "0.50.6")
     ],
@@ -43,3 +40,39 @@ let package = Package(
             dependencies: ["Lytics"])
     ]
 )
+
+// MARK: - Additional Dependencies for CI
+
+enum CI: String {
+    static let environmentVariable = "LYTICS_SWIFT_CI"
+
+    case release = "BUILDING_FOR_RELEASE"
+
+    init?(cString: UnsafeMutablePointer<CChar>?) {
+        guard let cString else {
+            return nil
+        }
+        self.init(rawValue: String(cString: cString))
+    }
+}
+
+let workflow: CI?
+#if canImport(Darwin)
+import Darwin
+workflow = CI(cString: getenv(CI.environmentVariable))
+#elseif canImport(Glibc)
+import Glibc
+workflow = CI(cString: getenv(CI.environmentVariable))
+#else
+workflow = nil
+#endif
+
+// Only require additional dependencies when needed for CI
+switch workflow {
+case .release:
+    package.dependencies += [
+        .package(url: "https://github.com/mobelux/swift-version-file-plugin", from: "0.1.0")
+    ]
+case .none:
+    break
+}
